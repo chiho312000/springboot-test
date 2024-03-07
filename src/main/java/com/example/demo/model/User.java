4b61bd21dd589377543c37a2fe956de673ec55b6
Rory <chiho312000@hotmail.com.hk>
2024-03-07 13:15:28 -0500

    Remove lcoal customer properties

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,16 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+public class User {
+    private String name;
+    private String gender;
+
+}
